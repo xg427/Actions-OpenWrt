e391d5402e4209e7319afba1b611b50530db12f9
root <xg427@hotmail.com>
2024-08-19 01:17:31 +0800

    --

--- custom-config.sh
+++ custom-config.sh
@@ -11,16 +11,16 @@ echo 'CONFIG_PACKAGE_git-http=y' >> $BUILDROOT/.config
 echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=3072' >> $BUILDROOT/.config
 
 # 添加luci-app-openclash
-echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> $BUILDROOT/.config
-echo '# CONFIG_PACKAGE_dnsmasq is not set' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_luci-compat=y' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_wget-ssl=y' >> $BUILDROOT/.config
+# echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> $BUILDROOT/.config
+# echo '# CONFIG_PACKAGE_dnsmasq is not set' >> $BUILDROOT/.config
+# echo 'CONFIG_PACKAGE_luci-compat=y' >> $BUILDROOT/.config
+# echo 'CONFIG_PACKAGE_wget-ssl=y' >> $BUILDROOT/.config
 
 # 添加luci-app-dockerman
-echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_dockerd=y' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_docker-compose=y' >> $BUILDROOT/.config
+# echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> $BUILDROOT/.config
+# echo 'CONFIG_PACKAGE_dockerd=y' >> $BUILDROOT/.config
+# echo 'CONFIG_PACKAGE_docker-compose=y' >> $BUILDROOT/.config
 
 # 添加其他app
-echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> $BUILDROOT/.config
+# echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> $BUILDROOT/.config
+# echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> $BUILDROOT/.config
